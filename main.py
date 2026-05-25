b11648a6d8990ac9ac3cab53d2bc5af1c865d7a1
OuOSama <real.ouosama@gmail.com>
2026-05-25 18:39:19 +0700

    ✨ feat(main) add CausalSelfAttention class

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
 import math
+from dataclasses import dataclass
+
+
+@dataclass
+class GPTConfig:
+    vocab_size: int
+    block_size: int
+    n_layer: int = 12
+    n_head: int = 12
+    n_embd: int = 768
+    dropout: float = 0.1
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-class LLM(nn.Module):
-    def __init__(self, config):
+class GPT2(nn.Module):
+    def __init__(self, config: GPTConfig):
+        super().__init__()
         # word token embedding (WTE)
         self.wte = nn.Embedding(config.vocab_size, config.n_embd)
         # word positional embedding (WPE)
@@ -21,7 +34,7 @@ class SingleHeadSelfAttention(nn.Module):
         # The single, fused linear layer for Q, K, V
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=False)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         B, T, C = x.size()
 
         # 1. Get Q, K, V from a single projection and split them
@@ -35,3 +48,44 @@ class SingleHeadSelfAttention(nn.Module):
         # 3. Aggregate values
         output = attention_weights @ v
         return output
+
+
+class CausalSelfAttention(nn.Module):
+    bias: torch.Tensor
+
+    def __init__(self, config: GPTConfig):
+        super().__init__()
+        assert config.n_embd % config.n_head == 0
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.register_buffer(
+            "bias",
+            torch.tril(torch.ones(config.block_size, config.block_size)).view(
+                1, 1, config.block_size, config.block_size
+            ),
+        )
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        B, T, C = x.size()
+
+        # 1. Get QKV and SPLIT into heads
+        qkv = self.c_attn(x)
+        q, k, v = qkv.split(self.n_embd, dim=2)
+
+        head_dim = C // self.n_head
+        q = q.view(B, T, self.n_head, head_dim).transpose(1, 2)  # (B, nh, T, hd)
+        k = k.view(B, T, self.n_head, head_dim).transpose(1, 2)  # (B, nh, T, hd)
+        v = v.view(B, T, self.n_head, head_dim).transpose(1, 2)  # (B, nh, T, hd)
+
+        # 2. Run causal self-ATTENTION on each head in parallel
+        att = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(head_dim))
+        att = att.masked_fill(self.bias[:, :, :T, :T] == 0, float("-inf"))
+        att = F.softmax(att, dim=-1)
+        y = att @ v  # (B, nh, T, hd)
+
+        # 3. MERGE heads and project
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        y = self.c_proj(y)
+        return y
