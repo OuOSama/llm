d706325cf2a0db94bfd13c8c92d0a03b602f860c
OuOSama <real.ouosama@gmail.com>
2026-06-20 11:03:54 +0700

    ✨ feat : add dowlaod shakespeare

---
+++ src/download_shakespeare.py
@@ -0,0 +1,8 @@
+# src/download_shakespeare.py
+import os
+import urllib.request
+
+os.makedirs("data", exist_ok=True)
+url = "https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt"
+urllib.request.urlretrieve(url, "data/raw_corpus.txt")
+print("downloaded ✅")
